094e818e57653314dfbe78921368598a1b51831b
Melissa Christiana <machristiana@ufl.edu>
2021-07-21 17:11:07 -0400

    Randomize js array

---
+++ src/main.js
@@ -0,0 +1,12 @@
+let netflixShows = ["Greys Anatomy", "Bridgeton", "Vampire Diaries", "New Girl", "Gilmore Girls", "Orange is the New Black", "NCIS", "Schitts Creek", "Criminal Minds", "The Queen's Gambit"]
+
+let netflixShow = document.getElementById("netflix-show");
+
+
+
+document.getElementById("button").addEventListener("click",
+    () => {
+        netflixShows.sort(function(a, b) { return 0.5 - Math.random() });
+        netflixShow.innerHTML = netflixShows[0];
+    }, false
+);
